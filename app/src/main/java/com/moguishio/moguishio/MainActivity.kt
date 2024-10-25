b66de3254b3c53b6f356fcefbb17a7b472487662
moguism <148863935+moguism@users.noreply.github.com>
2024-10-25 20:24:29 +0200

    DOCTOR EXTRAÑO

--- app/src/main/java/com/moguishio/moguishio/MainActivity.kt
+++ app/src/main/java/com/moguishio/moguishio/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.moguishio.moguishio.ui.theme.MoguishioTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +27,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MoguishioTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Greeting("Help", modifier = Modifier.fillMaxSize())
             }
         }
     }
@@ -32,10 +35,23 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    val image = painterResource(R.drawable.scale)
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = "Help"
+        )
+        Image (
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+        Text(
+            text = "LARGA VIDA A OLIVER"
+        )
+    }
 }
 
 @Preview(showBackground = true)
