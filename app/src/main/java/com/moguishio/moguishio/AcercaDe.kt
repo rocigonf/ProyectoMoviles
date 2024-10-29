faa55777021dac8e1621c02f0461705a4c1a2434
moguism <148863935+moguism@users.noreply.github.com>
2024-10-29 20:40:43 +0100

    BLUE LOBSTER JUMPSCARE

---
+++ app/src/main/java/com/moguishio/moguishio/AcercaDe.kt
@@ -0,0 +1,40 @@
+package com.moguishio.moguishio
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MainPage() {
+    val image = painterResource(R.drawable.scale)
+    Column (
+        modifier = Modifier.fillMaxSize(),
+    ){
+        Text(
+            text = "Help",
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.height(50.dp))
+        Image (
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+        Spacer(modifier = Modifier.height(50.dp))
+        Text(
+            text = "LARGA VIDA A OLIVER",
+            textAlign = TextAlign.Justify
+        )
+    }
+}
--- app/src/main/java/com/moguishio/moguishio/MainActivity.kt
+++ app/src/main/java/com/moguishio/moguishio/MainActivity.kt
@@ -4,60 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.moguishio.moguishio.ui.theme.MoguishioTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MoguishioTheme {
-                Greeting("Help", modifier = Modifier.fillMaxSize())
-            }
+            MainPage()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val image = painterResource(R.drawable.scale)
-    Column (
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Text(
-            text = "Help"
-        )
-        Image (
-            painter = image,
-            contentDescription = null,
-            contentScale = ContentScale.Crop
-        )
-        Text(
-            text = "LARGA VIDA A OLIVER"
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MoguishioTheme {
-        Greeting("Android")
     }
 }
