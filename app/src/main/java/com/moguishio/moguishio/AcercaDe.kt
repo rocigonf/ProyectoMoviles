101024c1c23b29b2a7fd161b5e24e4afc3ad6da9
moguism <148863935+moguism@users.noreply.github.com>
2024-10-29 20:43:12 +0100

    Ahora funciona el gitignore
    Por favor no me pongan dentro del archivo :(

--- app/src/main/java/com/moguishio/moguishio/AcercaDe.kt
+++ app/src/main/java/com/moguishio/moguishio/AcercaDe.kt
@@ -5,10 +5,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
