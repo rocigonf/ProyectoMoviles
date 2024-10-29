6612ab48ebdf89db5771aa9e0e839d7d183a3828
moguism <148863935+moguism@users.noreply.github.com>
2024-10-29 21:11:33 +0100

    Traducción incorporada

--- app/src/main/java/com/moguishio/moguishio/AcercaDe.kt
+++ app/src/main/java/com/moguishio/moguishio/AcercaDe.kt
@@ -1,23 +1,31 @@
 package com.moguishio.moguishio
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun MainPage() {
+    val context = LocalContext.current
     val image = painterResource(R.drawable.scale)
     Column (
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+            .padding(16.dp)
     ){
         Text(
             text = "Help",
@@ -31,7 +39,7 @@ fun MainPage() {
         )
         Spacer(modifier = Modifier.height(50.dp))
         Text(
-            text = "LARGA VIDA A OLIVER",
+            text = context.getString(R.string.loremipsum),
             textAlign = TextAlign.Justify
         )
     }
