761b092b8b1e4d35ac72fd851edf1abfcf8d1138
moguism <mauriciorosso.aboy@gmail.com>
2024-10-31 11:51:47 +0100

    Terminada vista de AcercaDe
    FIVE NIGHTS AT ROCIIIIIIIIIIIIIOS

--- app/src/main/java/com/moguishio/moguishio/MainActivity.kt
+++ app/src/main/java/com/moguishio/moguishio/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.moguishio.moguishio.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MainPage()
+            AppTheme {
+                MainPage()
+            }
         }
     }
 }
