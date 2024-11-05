40025c5e5072362f058c4d7f927d111668545d97
moguism <148863935+moguism@users.noreply.github.com>
2024-11-05 19:26:55 +0100

    hehe

--- app/src/main/java/com/moguishio/moguishio/MainActivity.kt
+++ app/src/main/java/com/moguishio/moguishio/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.moguishio.moguishio
 
 import android.content.res.Configuration
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -21,6 +22,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.moguishio.moguishio.ui.theme.AppTheme
@@ -32,36 +35,54 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppTheme {
+                val mediaPlayer = MediaPlayer.create(LocalContext.current, R.raw.sound)
                 var change by remember { mutableStateOf(true) }
                 Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
                     modifier = Modifier
-                        .fillMaxSize()
                         .background(MaterialTheme.colorScheme.inversePrimary)
-                ) {
-                    Spacer(modifier = Modifier.height(20.dp))
+                )
+                {
                     Button(
-                        onClick = {
-                            change = !change
+                        onClick =
+                        {
+                            //change = !change
+                            mediaPlayer.start()
                         },
-                        shape = MaterialTheme.shapes.medium,
-                        colors = ButtonDefaults.buttonColors(
-                            containerColor = MaterialTheme.colorScheme.primaryContainer,
-                            contentColor = MaterialTheme.colorScheme.primary
-                        )
+                        modifier = Modifier.alpha(0f)
                     ) {
                         Text(
-                            text = getString(R.string.changeView),
+                            text = "buton",
                             style = AppTypography.headlineSmall
                         )
                     }
-                    if(change)
-                    {
-                        MainPage()
-                    }
-                    else
-                    {
-                        SobreNosotros()
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(MaterialTheme.colorScheme.inversePrimary)
+                    ) {
+
+                        Spacer(modifier = Modifier.height(20.dp))
+                        Button(
+                            onClick = {
+                                change = !change
+                            },
+                            shape = MaterialTheme.shapes.medium,
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                contentColor = MaterialTheme.colorScheme.primary
+                            )
+                        ) {
+                            Text(
+                                text = getString(R.string.changeView),
+                                style = AppTypography.headlineSmall
+                            )
+                        }
+                        if (change) {
+                            MainPage()
+                        } else {
+                            SobreNosotros()
+                        }
                     }
                 }
             }
