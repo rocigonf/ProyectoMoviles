f0ffbdfca47e1311c739d17a525b323b9883a0c9
moguism <148863935+moguism@users.noreply.github.com>
2024-11-29 18:14:54 +0100

    rapsodia bohemia

--- app/src/main/java/com/moguishio/moguishio/model/films/ClientePelicula.kt
+++ app/src/main/java/com/moguishio/moguishio/model/films/ClientePelicula.kt
@@ -3,6 +3,6 @@ package com.moguishio.moguishio.model.films
 import retrofit2.http.GET
 
 interface ClientePelicula {
-    @GET("/v3/5a03a760-48dd-4b1f-b7f8-c0d3ae675ea6")
+    @GET("/v3/41951a99-43aa-453f-b382-60eb72063c02")
     suspend fun getFilms(): List<DatosPelicula>
 }
