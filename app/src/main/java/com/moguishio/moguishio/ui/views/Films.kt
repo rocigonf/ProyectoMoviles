f829987991b7c1a75097e244c0d5d99ad5490511
moguism <148863935+moguism@users.noreply.github.com>
2024-11-29 17:06:51 +0100

    Cambiado circulito porque es cool😎

--- app/src/main/java/com/moguishio/moguishio/ui/views/Films.kt
+++ app/src/main/java/com/moguishio/moguishio/ui/views/Films.kt
@@ -5,17 +5,18 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+//import androidx.compose.ui.text.style.TextAlign
 import androidx.navigation.NavHostController
 import com.moguishio.moguishio.model.films.DatosPelicula
 import com.moguishio.moguishio.ui.components.BotonVolver
-import com.moguishio.moguishio.ui.components.EstablecerTexto
+//import com.moguishio.moguishio.ui.components.EstablecerTexto
 import com.moguishio.moguishio.ui.components.FilmItem
 import com.moguishio.moguishio.viewmodel.ViewModelPelicula
 
@@ -31,7 +32,8 @@ fun Films(navController: NavHostController, context: Context, viewModel: ViewMod
         BotonVolver(navController, context)
         if (films.isEmpty()) {
             // Muestra el indicador de cargando
-            EstablecerTexto(text = "Loading...", textAlign = TextAlign.Center)
+            CircularProgressIndicator()
+            //EstablecerTexto(text = "Loading...", textAlign = TextAlign.Center)
         } else {
             // Muestra la lista de columnas :D
             FilmList(films)
