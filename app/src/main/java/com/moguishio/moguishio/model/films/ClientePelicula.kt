ab6f84d002e891a9af49337d1492fcb6430416f6
Rocío Gonfaus Luengo <116120484+rocigonf@users.noreply.github.com>
2025-01-14 21:14:33 +0100

    Update ClientePelicula.kt

--- app/src/main/java/com/moguishio/moguishio/model/films/ClientePelicula.kt
+++ app/src/main/java/com/moguishio/moguishio/model/films/ClientePelicula.kt
@@ -3,6 +3,6 @@ package com.moguishio.moguishio.model.films
 import retrofit2.http.GET
 
 interface ClientePelicula {
-    @GET("/v3/731a11c6-ae32-4b77-adcb-b5eabb4cc652")
+    @GET("/v3/e25e8095-9132-4220-8007-414f5c783a4a")
     suspend fun getFilms(): List<DatosPelicula>
 }
